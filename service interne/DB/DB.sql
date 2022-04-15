c63bd2b4fb2c83082fd3f27143481ba2308deba7
quentinrld <71373028+quentinrld@users.noreply.github.com>
2022-04-15 15:46:07 +0200

    Create DB.sql

---
+++ service interne/DB/DB.sql
@@ -0,0 +1,12 @@
+CREATE DARABASE 'Woodytoys'
+
+CREATE TABLE 'articles' (
+  'ID' INTEGER NOT NULL AUTO_INCREMENT,
+  'article' varchar(90) NOT NULL,
+  'prix' INTEGER NOT NULL,
+  CONSTRAINT 'pk_articles' PRIMERY KEY (ID);
+)
+
+CREATE USER admin IDENTIFIED WITH mysql_native_password BY 'password'
+
+GRANT ALL PRIVILEGES ON Woodytoys.* TO admin 
