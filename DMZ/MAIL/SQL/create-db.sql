2bdaeb6afe827a48ad4d846fabc8a24f24cb8d9e
craftbrak <louisdewilde2001@gmail.com>
2022-08-29 09:46:23 +0200

    config mail

--- DMZ/MAIL/SQL/create-db.sql
+++ DMZ/MAIL/SQL/create-db.sql
@@ -18,7 +18,6 @@ CREATE TABLE `virtual_users` (
 `domain_id` INT NOT NULL,
 `password` VARCHAR(106) NOT NULL,
 `email` VARCHAR(120) NOT NULL,
-`maildir` VARCHAR(120) NOT NULL,
 PRIMARY KEY (`id`),
 UNIQUE KEY `email` (`email`),
 FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
@@ -31,27 +30,27 @@ VALUES
     ('1', 'woodytoys.louisdewilde.be');
 
 INSERT INTO `mailserver`.`virtual_users`
-(`id`, `domain_id`, `password` , `email`, `maildir`)
+(`id`, `domain_id`, `password` , `email`)
 VALUES
-    ('1', '1', ENCRYPT('pw_louis', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'louis@woodytoys.louisdewilde.be', 'woodytoys.louisdewilde.be/sebastien/');
+    ('1', '1', ENCRYPT('pw_louis', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'louis@woodytoys.louisdewilde.be');
 
 INSERT INTO `mailserver`.`virtual_users`
-(`id`, `domain_id`, `password` , `email`, `maildir`)
+(`id`, `domain_id`, `password` , `email`)
 VALUES
-    ('2', '1', ENCRYPT('pw_tommy', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'tommy@woodytoys.louisdewilde.be', 'woodytoys.louisdewilde.be/tommy/');
+    ('2', '1', ENCRYPT('pw_tommy', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'tommy@woodytoys.louisdewilde.be');
 
 INSERT INTO `mailserver`.`virtual_users`
-(`id`, `domain_id`, `password` , `email`, `maildir`)
+(`id`, `domain_id`, `password` , `email`)
 VALUES
-    ('3', '1', ENCRYPT('pw_quentin', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'quentin@woodytoys.louisdewilde.be', 'woodytoys.louisdewilde.be/quentin/');
+    ('3', '1', ENCRYPT('pw_quentin', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'quentin@woodytoys.louisdewilde.be');
 
 INSERT INTO `mailserver`.`virtual_users`
-(`id`, `domain_id`, `password` , `email`, `maildir`)
+(`id`, `domain_id`, `password` , `email`)
 VALUES
-    ('4', '1', ENCRYPT('pw_B2B', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'b2b@woodytoys.louisdewilde.be', 'woodytoys.louisdewilde.be/b2b/');
+    ('4', '1', ENCRYPT('pw_B2B', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'b2b@woodytoys.louisdewilde.be');
 
 INSERT INTO `mailserver`.`virtual_users`
-(`id`, `domain_id`, `password` , `email`, `maildir`)
+(`id`, `domain_id`, `password` , `email`)
 VALUES
-    ('5', '1', ENCRYPT('pw_Contact', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'contact@woodytoys.louisdewilde.be', 'woodytoys.louisdewilde.be/contact/');
+    ('5', '1', ENCRYPT('pw_Contact', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'contact@woodytoys.louisdewilde.be');
 
